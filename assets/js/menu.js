905e2f77f20d508c0005ac39efcb2b09baf6288b
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-30 11:49:16 +0200

    Add files via upload

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,22 @@
-document.querySelector('.burger-menu').addEventListener('click', function() {
+// Открытие и закрытие меню при клике на бургер
+document.querySelector('.burger-menu').addEventListener('click', function(event) {
+    const menu = document.querySelector('.menu__open');
+    const burger = document.querySelector('.burger-menu');
+
+    // Избегаем закрытия меню, если клик был по самому бургеру
+    menu.classList.toggle('active');
+    burger.classList.toggle('active');
+});
+
+// Закрытие меню при клике на свободное место
+document.addEventListener('click', function(event) {
     const menu = document.querySelector('.menu__open');
     const burger = document.querySelector('.burger-menu');
     
-    menu.classList.toggle('active'); 
-    burger.classList.toggle('active'); 
-  });
+    // Проверяем, был ли клик внутри меню или по бургеру
+    if (!menu.contains(event.target) && !burger.contains(event.target)) {
+        // Если клик был вне меню и бургера, закрываем меню
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+    }
+});
